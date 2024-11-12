a816e4d4f1e518a52b447d1efac1ecfbc6717707
JoseAguilar <joseaguilarsalazar2004@gmail.com>
2024-11-12 16:34:26 -0500

    feat: item data in generateDataForFacturacion test11

--- pdf/scripts/comprobanteA4.py
+++ pdf/scripts/comprobanteA4.py
@@ -116,7 +116,7 @@ def generate_pdf(bucket_name, s3_key, imagePath, data):
         c.drawString(1.5 * cm, currentY , str(value[0]))
         c.drawString(3.3 * cm, currentY ,str(value[1]))
         c.drawString(5.5 * cm, currentY ,str(value[2]))
-        paragrph.drawOn(c, 8*cm, currentY - paragrph.height + 10)
+        paragrph.drawOn(c, 8*cm, currentY - paragrph.height + 20)
         c.drawString(17.5 * cm, currentY , str(value[4]))
         c.drawString(19 * cm, currentY , str(value[4] * value[0]))
         
